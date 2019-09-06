b1dba7786a6d1b29c743cfc0828c98b16b072bc9
Levon Ayvazyan <LevonAyvazyan43@gmail.com>
2019-09-06 18:23:35 +0400

    basic token endpoint [incomplete]

--- server/routes/api.php
+++ server/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,21 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+//Route::middleware('auth')->get('/user', function (Request $request) {
+//    return $request->user();
+//});
+
+Route::group([
+
+    'middleware' => 'api',
+    'prefix' => 'auth'
+
+], function ($router) {
+
+    Route::post('login', 'AuthController@login')->name('login');
+    Route::post('logout', 'AuthController@logout');
+    Route::post('refresh', 'AuthController@refresh');
+    Route::get('me', 'AuthController@me');
+
 });
+
