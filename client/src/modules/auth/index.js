37b943fb8dc88197e88ec93fec9b227633a20999
Levon Ayvazyan <LevAyv43@gmail.com>
2019-09-10 18:17:43 +0400

    client - axios to endpoint /api/auth/login without Redux

--- client/src/modules/auth/index.js
+++ client/src/modules/auth/index.js
@@ -1,15 +1,27 @@
 import React from "react";
 import LabeledInput from "../common/LabeledInput";
+import Button from "../common/Button";
+import axios from "axios";
+
+//  todo * connect redux store
+//  todo * add button
+//  todo * button click must make request
+//  todo ** request endpoint is localhost:8000/api/auth/login
+//  todo ** request must have specified headers [see postman]
+//  todo ** request must have body credentials
+//
 
 class LoginPage extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
+            /** user input of corresponding fields */
             credentials: {
                 login: "",
                 password: "",
             }
         };
+        /** user input inside LOGIN field */
         this.updateLogin = (event) => {
             const newLoginValue = event.target.value;
             this.setState(state =>
@@ -21,6 +33,7 @@ class LoginPage extends React.Component {
                 }));
             console.log("!", this.state)
         };
+        /** user input inside PASSWORD field */
         this.updatePassword = (event) => {
             const newPasswordValue = event.target.value;
             this.setState(state =>
@@ -32,10 +45,31 @@ class LoginPage extends React.Component {
                 }));
             console.log("!", this.state)
         };
+        /** user click on LOGIN button */
+        this.handleLogin = () => {
+            axios.post(
+                "http://" + process.env.REACT_APP_BACKEND_IP_PORT + "/api/auth/login",
+                {
+                    email: this.state.credentials.login,
+                    password: this.state.credentials.password,
+                },
+                {
+                    headers: {
+                        "Content-Type": "application/json"
+                    }
+                }
+            ).then((/*response*/) => {
+                // console.log(response.status);
+                // console.log(response.data);
+                //todo add dispatch to store
+
+            }).catch(error => {
+                console.log(`[ERROR CODE:${error.response.status}] ${error.response.data.error}`);
+            });
+        }
     }
 
     // todo for testing only
-
     // componentDidUpdate(prevProps, prevState, snapshot) {
     //     console.log(" --- Component Did Update --- ")
     //     console.log("current / previous state")
@@ -62,6 +96,10 @@ class LoginPage extends React.Component {
                     placeholder={"1234"}
                     onChange={this.updatePassword}
                     value={this.state.credentials.password}
+                />
+                <Button
+                    displayName={"Sign-in"}
+                    onClick={this.handleLogin}
                 />
             </div>
         );
