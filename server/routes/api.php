a4295921d456df0437d4108a3bf1bcc69a132e65
Levon Ayvazyan <LevAyv43@gmail.com>
2019-09-10 11:59:22 +0400

    server , corrupted "routes" >> "route" fix

--- server/routes/api.php
+++ server/routes/api.php
@@ -8,8 +8,8 @@ use Illuminate\Support\Facades\Route;
 | API Routes
 |--------------------------------------------------------------------------
 |
-| Here is where you can register API route for your application. These
-| route are loaded by the RouteServiceProvider within a group which
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
@@ -23,9 +23,9 @@ Route::group([
     'middleware' => 'api',
     'prefix' => 'auth'
 
-], function (/*$router*/) {
+], function ($router) {
 
-    Route::post('login', 'AuthController@login')/*->name('login')*/;
+    Route::post('login', 'AuthController@login')->name('login');
     Route::post('logout', 'AuthController@logout');
     Route::post('refresh', 'AuthController@refresh');
     Route::get('me', 'AuthController@me');
