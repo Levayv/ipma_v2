41eb93c90990d650efca90562c0fda35bbcbb02c
Levon Ayvazyan <LevAyv43@gmail.com>
2019-09-18 19:30:22 +0400

    client - crud[create] LessonForm route and skeleton

---
+++ client/src/modules/lesson/form/LessonForm.js
@@ -0,0 +1,18 @@
+import React, {Component} from "react";
+// import {connect} from "react-redux";
+
+// import {crud_read} from "../../../redux/action";
+
+// import LabeledInput from "./components/LabeledInput";
+// import FormSubmitButton from "./components/FormSubmitButton";
+// import history from "../../../route/history";
+
+class LessonForm extends Component {
+    render() {
+        return (
+            <div> Form </div>
+        )
+    }
+}
+
+export default LessonForm;
--- client/src/route/index.js
+++ client/src/route/index.js
@@ -8,8 +8,9 @@ import {
 
 import history from "./history";
 import LoginPage from "../modules/auth";
+import LessonForm from "../modules/lesson/form/LessonForm";
+
 // import LessonList from "./modules/lesson/list/LessonList";
-// import LessonForm from "./modules/lesson/form/LessonForm";
 // import LessonDelete from "./modules/lesson/form/LessonDelete";
 
 import initialize from "../config/init";
@@ -50,7 +51,7 @@ class AppRouter extends Component {
 
                     <Route path="/" exact component={Dashboard}/>
                     <Route path="/auth" component={Auth}/>
-                    {/*<Route path="/lesson" component={Lesson}/>*/}
+                    <Route path="/lesson" component={Lesson}/>
                 </div>
             </Router>
         );
@@ -69,20 +70,19 @@ function Auth({match}) {
         <Route exact path={`${match.path}/login`} component={LoginPage}/>
     </div>)
 }
-// function Lesson({match}) {
-//     return (<div>
-//         <Route exact path={`${match.path}/auth/login`} component={LoginPage}/>
-//         {/*<Route exact path={`${match.path}/list/`} component={LessonList}/>*/}
-//         {/*<Route exact path={`${match.path}/form/`} component={LessonForm}/>*/}
-//         {/*<Route exact path={`${match.path}/edit/`}*/}
-//         {/*       render={() => {*/}
-//         {/*           alert("Please use edit button inside List of Lessons");*/}
-//         {/*           // history.push("/lesson/list")*/}
-//         {/*           return (<Redirect to={{pathname: "/lesson/list",}}/>)*/}
-//         {/*       }}/>*/}
-//         {/*<Route path={`${match.path}/edit/:recordID`} component={LessonForm}/>*/}
-//         {/*<Route path={`${match.path}/delete/:recordID`} component={LessonDelete}/>*/}
-//     </div>)
-// }
+function Lesson({match}) {
+    return (<div>
+        <Route exact path={`${match.path}/form/`} component={LessonForm}/>
+        {/*<Route exact path={`${match.path}/list/`} component={LessonList}/>*/}
+        {/*<Route exact path={`${match.path}/edit/`}*/}
+        {/*       render={() => {*/}
+        {/*           alert("Please use edit button inside List of Lessons");*/}
+        {/*           // history.push("/lesson/list")*/}
+        {/*           return (<Redirect to={{pathname: "/lesson/list",}}/>)*/}
+        {/*       }}/>*/}
+        {/*<Route path={`${match.path}/edit/:recordID`} component={LessonForm}/>*/}
+        {/*<Route path={`${match.path}/delete/:recordID`} component={LessonDelete}/>*/}
+    </div>)
+}
 
 export default AppRouter;
