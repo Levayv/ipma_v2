3ddd98d5cfa0112df08a5cd71ced67b51cea0930
Levon Ayvazyan <LevAyv43@gmail.com>
2019-10-08 19:27:58 +0400

    client - AppRouter upgrade: +bootstrap +refactored

--- client/src/route/AppRouter.js
+++ client/src/route/AppRouter.js
@@ -7,6 +7,10 @@ import {
     Redirect,
 } from "react-router-dom";
 
+import Container from 'react-bootstrap/Container';
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+
 import history from "./history";
 import LoginPage from "../modules/auth/LoginPage";
 import LessonAddForm from "../modules/lesson/form/Add";
@@ -14,8 +18,12 @@ import Dashboard from "../modules/dashboard/Dashboard";
 import LessonEditForm from "../modules/lesson/form/Edit";
 import LessonList from "../modules/lesson/list/LessonList";
 import {loadTokenFromLocalStorage} from "../api/mapStoreToLocal";
-
 import initialize from "../config/init";
+
+import './Testing.css';
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import NavDropdown from "react-bootstrap/NavDropdown";
 
 
 class AppRouter extends Component {
@@ -33,55 +41,76 @@ class AppRouter extends Component {
         const renderAvailableLinks = () => {
             // todo remove temp solution (activeStyle)
             return (
-                <nav className={"router-nav-menu"}>
-                    <ul>
-                        <li id={"router-nav-home"} className={"router-nav-item"}>
-                            <NavLink
-                                activeClassName="router-nav-link-selected"
-                                to="/home"
-                                activeStyle={{fontWeight: "bold", color: "blue"}}
-                            >
-                                HomePage
-                            </NavLink>
-                        </li>
-                        <li id={"router-nav-login"} className={"router-nav-item"}>
-                            <NavLink
-                                activeClassName="router-nav-link-selected"
-                                to="/auth/login"
-                                activeStyle={{fontWeight: "bold", color: "blue"}}
-                            >
-                                Login
-                            </NavLink>
-                        </li>
-                        <li id={"router-nav-dashboard"} className={"router-nav-item"}>
-                            <NavLink
-                                activeClassName="router-nav-link-selected"
-                                to="/dashboard"
-                                activeStyle={{fontWeight: "bold", color: "blue"}}
-                            >
-                                Dashboard
-                            </NavLink>
-                        </li>
-                        <li id={"router-nav-list"} className={"router-nav-item"}>
-                            <NavLink
-                                activeClassName="router-nav-link-selected"
-                                to="/lesson/list"
-                                activeStyle={{fontWeight: "bold", color: "blue"}}
-                            >
-                                List
-                            </NavLink>
-                        </li>
-                        <li id={"router-nav-form"} className={"router-nav-item"}>
-                            <NavLink
-                                activeClassName="router-nav-link-selected"
-                                to="/lesson/form"
-                                activeStyle={{fontWeight: "bold", color: "blue"}}
-                            >
-                                Form
-                            </NavLink>
-                        </li>
-                    </ul>
-                </nav>
+                <Navbar
+                    className={'router-nav-menu'}
+                    expand='true'
+                    variant='light'
+                    bg='light'
+                >
+                    <Navbar.Brand href="#home"> &#9875; </Navbar.Brand>
+                    <Navbar.Toggle aria-controls="main-navbar-root"/>
+                    <Navbar.Collapse id="main-navbar-root">
+                        <Nav className="mr-auto">
+                            {/*<Nav.Link href="">Home</Nav.Link>*/}
+                            {/*<Nav.Link href="">Link</Nav.Link>*/}
+
+
+                            <Nav.Link href=''>
+                                <NavLink
+                                    activeClassName="router-nav-link-selected"
+                                    to="/home"
+                                    activeStyle={{fontWeight: "bold", color: "black"}}
+                                >
+                                    HomePage
+                                </NavLink>
+                            </Nav.Link>
+                            <Nav.Link href=''>
+                                <NavLink
+                                    activeClassName="router-nav-link-selected"
+                                    to="/auth/login"
+                                    activeStyle={{fontWeight: "bold", color: "black"}}
+                                >
+                                    Login
+                                </NavLink>
+                            </Nav.Link>
+                            <Nav.Link href=''>
+                                <NavLink
+                                    activeClassName="router-nav-link-selected"
+                                    to="/dashboard"
+                                    activeStyle={{fontWeight: "bold", color: "black"}}
+                                >
+                                    Dashboard
+                                </NavLink>
+                            </Nav.Link>
+
+                            <NavDropdown title="Lesson" id="lesson-nav-dropdown">
+                                <NavDropdown.Item href="">
+                                    <Nav.Link href=''>
+                                        <NavLink
+                                            activeClassName="router-nav-link-selected"
+                                            to="/lesson/list"
+                                            activeStyle={{fontWeight: "bold", color: "black"}}
+                                        >
+                                            List
+                                        </NavLink>
+                                    </Nav.Link>
+                                </NavDropdown.Item>
+                                <NavDropdown.Item href="">
+                                    <Nav.Link href=''>
+                                        <NavLink
+                                            activeClassName="router-nav-link-selected"
+                                            to="/lesson/form"
+                                            activeStyle={{fontWeight: "bold", color: "black"}}
+                                        >
+                                            Form
+                                        </NavLink>
+                                    </Nav.Link>
+                                </NavDropdown.Item>
+                                <NavDropdown.Divider/>
+                            </NavDropdown>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Navbar>
 
 
             )
@@ -105,11 +134,50 @@ class AppRouter extends Component {
         return (
             <Router history={history}>
                 <div className={"router-root"}>
-                    {renderAvailableLinks()}
-                    {renderAvailableRoutes()}
+                    <Container>
+                        <Row>
+                            <Col xs={12} sm={4} md={3} lg={2} className={"aside-root"}>
+                                <Row className={'aside-body'}>
+                                    {renderAvailableLinks()}
+                                </Row>
+                            </Col>
+                            <Col xs={12} sm={8} md={9} lg={10} className={"main-root"}>
+                                <Row>
+                                    <Col className={"main-header"}>
+                                        breadcrumbs
+                                        <span> Width / Height =  </span>
+                                        <span> {this.state.width}</span>
+                                        <span>  / </span>
+                                        <span> {this.state.height}</span>
+                                    </Col>
+                                </Row>
+                                <Row>
+                                    <Col className={"main-body"}>
+                                        {renderAvailableRoutes()}
+                                    </Col>
+                                </Row>
+                                <Row>
+                                    <Col className={"main-footer"}>
+                                            footer - something useful must be rendered here
+                                    </Col>
+                                </Row>
+                            </Col>
+                        </Row>
+                    </Container>
+
+                    {/*{renderAvailableLinks()}*/}
+                    {/*{renderAvailableRoutes()}*/}
                 </div>
             </Router>
         );
+    }
+
+    renderSizeAbbr() {
+        if (this.state.width >= 1200) return (<span> XL </span>)
+        if (this.state.width >= 992) return (<span> LG </span>)
+        if (this.state.width >= 768) return (<span> MD </span>)
+        if (this.state.width >= 576) return (<span> SM </span>)
+        return (<span> XS </span>)
     }
 }
 
